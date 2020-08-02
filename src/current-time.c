c8c08c4efcf81842aceb5b65ae0f1dd945dadce8
kiran santosh upase <kvsupase@gmail.com>
2020-08-02 06:56:43 +0530

    current time

---
+++ src/current-time.c
@@ -0,0 +1,22 @@
+/*program for current time*/
+#include <time.h>
+#include <stdio.h>  
+#include <stdlib.h>
+int main(void){
+    time_t cur_time;
+    char* cur_t_string;
+    cur_time = time(NULL);
+    if (cur_time == ((time_t)-1))
+    {
+        (void) fprintf(stderr, "Failure to get the current time.\n");
+        exit(EXIT_FAILURE);
+    }
+    cur_t_string = ctime(&cur_time); //convert to local time format
+    if (cur_t_string == NULL)
+    {
+        (void) fprintf(stderr, "Failure to convert the current time.\n");
+        exit(EXIT_FAILURE);
+    }
+    (void) printf("\nThe Current time is : %s \n", cur_t_string);
+    exit(EXIT_SUCCESS);
+}
