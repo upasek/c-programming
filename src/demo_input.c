7a77a24a37ee3009af362b765d28151b839c2e5c
kiran santosh upase <kvsupase@gmail.com>
2020-08-04 07:12:49 +0530

    input from user

---
+++ src/demo_input.c
@@ -0,0 +1,11 @@
+/*Program for taking input from user and print that input*/
+#include <stdio.h>
+int main() {
+
+    int input = 0;
+    printf("Enter any number : ");
+    scanf("%d", &input);
+    printf("Your number = %d\n", input);
+
+    return 0;
+}
