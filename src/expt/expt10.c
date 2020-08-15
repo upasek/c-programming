678f6d716ab8f7bb1f0c484ca8e618545837bf45
kiran santosh upase <kvsupase@gmail.com>
2020-08-15 11:51:09 +0530

    condition

---
+++ src/expt/expt10.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main() {
+    int i=65;
+    char j='A';
+    printf("The value of i is %d\n",i);
+    printf("The value of j is %c\n",j);
+    if (i==j)
+        printf("C is cow\n");
+    else 
+        printf("C is headache\n");
+    
+    return 0;
+}
