6269772813eeac94a918c40fc82b73d32a285da9
kiran santosh upase <kvsupase@gmail.com>
2020-08-16 13:09:50 +0530

    condition

---
+++ src/expt/expt12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    
+    int k=10,j=12;
+
+    printf("The value of k is %d ",k);
+    printf("\nThe value of j is %d ",j);
+    if (k >= j)
+        printf("\nk are good\n");
+    else 
+        printf("\nk are bad\n");
+
+    return 0;
+}
