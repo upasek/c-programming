1140b25e512e7bfeb17307825de1ee8c2f44d938
kiran santosh upase <kvsupase@gmail.com>
2020-08-18 15:15:45 +0530

    condition

---
+++ src/expt/expt16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int x = 30,y = 40;
+    printf("The value of x is %d.",x);
+    printf("\nThe value of y is %d.",y);
+    if (x == y)
+        printf("\nx is equal to y.\n");
+    else if (x > y)
+        printf("\nx is greater than y.\n");
+    else  (x < y);
+        printf("\nx is less than y.\n");
+
+    return 0;
+}
