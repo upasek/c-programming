a77e7205b6b9f97dfbb03809fa34d899e1d84be9
kiran santosh upase <kvsupase@gmail.com>
2020-08-18 15:02:11 +0530

    condition

---
+++ src/expt/expt15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+
+    int x = 10,y = 15;
+    printf("The value of x is %d .",x);
+    printf("\nThe value of y is %d .",y);
+    if (x % 2 == y % 3)
+        printf("\ncarpathians\n");
+    else
+        printf("kiran\n");
+
+    return 0;
+}
