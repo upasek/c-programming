024cf3fadf6b1f798af0eb388e09d822f2dbcbcf
kiran santosh upase <kvsupase@gmail.com>
2020-08-21 16:16:09 +0530

    decimal number

---
+++ Numbers/decimal.c
@@ -0,0 +1,21 @@
+/*convert binaty number into decimal number*/
+#include<stdio.h>
+int main (){
+    int num, q, b = 1, i = 1, a = 0, rev = 0, p;
+    printf("Enter the Binary Number : ");
+    scanf("%d",&num);
+    while ( num > 0 ){
+        q = num % 10;
+        if ( i == 1)
+            p = 1;
+        else 
+            p = 2;
+
+        b = b * p;
+        rev = rev + ( q * b);
+        num = num / 10;
+        i++;
+    }
+    printf("The equivalent Decimal Number : %d\n",rev);
+    return 0;
+}
