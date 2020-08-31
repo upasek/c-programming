bc728018b5316415d98ebf21d701cbd81435743b
kiran santosh upase <kvsupase@gmail.com>
2020-08-31 17:39:57 +0530

    sum of elements

---
+++ src/h.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main() {
+
+    /* finding sum of five digit number */
+
+
+    int a,b,c,d,e,sum ;
+    printf ("enter any value of a,b,c,d,e : ");
+    scanf ("%d%d%d%d%d",&a,&b,&c,&d,&e);
+
+
+    /* formula for sum a+b+c+d+e */
+
+
+    sum = a+b+c+d+e ;
+    printf ("The sum of all elements is : %d\n",sum);
+
+
+}
