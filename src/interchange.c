15e1c97431faa25a467541158ea041949fbabfce
kiran santosh upase <kvsupase@gmail.com>
2020-09-09 22:00:17 +0530

    interchange

---
+++ src/interchange.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main () {
+    int C,D ;
+
+    /* for interchangeing the value of C and D */
+
+    printf ("\nenter the value of C and D =  " );
+
+    scanf ("%d%d",&C,&D);
+
+    printf ("C = %d ", D);
+
+    printf ("D = %d ", C);
+
+}
