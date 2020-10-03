24524296670967cc2f98671628f2bbfc2cdabf80
kiran santosh upase <kvsupase@gmail.com>
2020-10-03 09:20:54 +0530

    lucas number

---
+++ Numbers/lucas-num.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main (){
+    int i, num, temp, p = 2, q = 1;
+    printf("Input number of terms to display : ");
+    scanf("%d",&num);
+    printf ("\nHere is the Fibonacci series upto to %d terms :",num);
+    for( i = 1; i <= num; i++){
+        if( i == 1)
+            printf(" %d ",p);
+        else if( i == 2)
+            printf(" %d ",q);
+        else{
+            printf(" %d ",p+q);
+            temp = q;
+            q = p + q;
+            p = temp;
+        }
+    }
+    printf("\n");
+}
+
