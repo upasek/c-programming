110cad03c25dcbdd92ec8a7f861f6f2578ce096b
kiran santosh upase <kvsupase@gmail.com>
2020-12-17 22:51:17 +0530

    practice 2

---
+++ src/practice1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+      int a = 0;
+      printf  ("\n%d\n",a++ - ++a);
+
+    /* int i = 33, j;
+     j = i++;
+     printf ("\n%d",j );
+    
+    j = ++i;
+    printf ("\n%d",j);*/
+
+}
