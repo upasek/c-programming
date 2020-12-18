442e9ffe081ce6e9c252d428a79b6301fa89c799
kiran santosh upase <kvsupase@gmail.com>
2020-12-18 23:16:22 +0530

    infinite loop

---
+++ loops/practicewhile.c
@@ -0,0 +1,16 @@
+//infinite loop
+
+#include <stdio.h>
+void main () {
+
+    int i=1;    // -32767 to +32767 
+
+    while ( 1 ){
+
+        printf ("\n i=%d",i);
+
+        i = i + 1 ;
+
+    }
+
+}
