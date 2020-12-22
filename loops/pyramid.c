bf9ba7be45d6e1ed010c30aeb3bb791b96860ca1
kiran santosh upase <kvsupase@gmail.com>
2020-12-22 22:02:03 +0530

    pyramid

---
+++ loops/pyramid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main (){
+    int i, j, k, n, l = 0;
+    printf ("Enter the number of rows :");
+    scanf ("%d",&n);
+
+    for ( i = 1; i <= n; i++ ){ 
+        for( j = n; j > i; j-- ){
+            printf (" ");
+
+        }
+
+        for ( k = 1; k <= i + l; k++ ){
+            printf ("*");
+        }
+        l++;
+        printf ("\n");
+    }
+}
