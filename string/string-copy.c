093d673fbba2c7b671ec2bce1b097c40f558b164
kiran santosh upase <kvsupase@gmail.com>
2021-03-20 23:49:44 +0530

    copy one string to another string

---
+++ string/string-copy.c
@@ -0,0 +1,21 @@
+/*Write a program in C to copy one string to another string. */
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main () {
+	char a[50];
+	char b[50];
+	printf("Input the string :");
+	fgets(a, sizeof a, stdin );
+
+	int i = 0;
+	while( a[i] != '\0' ){
+		b[i] = a[i];
+		i++;
+	}
+	b[i] = '\0';
+	printf("The frist string is : %s\n",a);
+	printf("The second string is : %s\n",b);
+	printf("Number of characters copied : %d\n",i);
+	return 0;
+}
